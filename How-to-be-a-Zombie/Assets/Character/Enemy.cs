a56f2c4d64d87d204efcf8fa7ee1273aa0f9a202
VuiTung <64191117+VuiTung@users.noreply.github.com>
2021-08-03 20:51:28 +0800

    little update

--- How-to-be-a-Zombie/Assets/Character/Enemy.cs
+++ How-to-be-a-Zombie/Assets/Character/Enemy.cs
@@ -6,7 +6,6 @@ public class Enemy : MonoBehaviour
 {
     public float MaxHP;
     float CurrentHP;
-    
     void Start()
     {
         CurrentHP = MaxHP; // assign hp to max hp
@@ -16,8 +15,12 @@ public class Enemy : MonoBehaviour
             CurrentHP -= damage;
             Debug.Log("Bonked " + this.name + ", " + CurrentHP + " left");
             if (CurrentHP <= 0) {
-                Destroy(this);
-                Debug.Log(this.name + " is dead");
-            }
+            //Destroy(this);
+            Debug.Log(this.name + " is dead");
+            GetComponent < follow2 > ().enabled = true;
+            // vv help change their layer here willis vv
+            //gameObject.layer = 2;
+            this.enabled = false;
+        }
         }
 }
